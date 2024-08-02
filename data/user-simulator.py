6a757ea4431804cd1fe9230b483cf0d22dda6114
Sebastian Joerz <sebastian.joerz@stud.h-da.de>
2024-08-02 08:43:23 +0200

    t1

--- data/user-simulator.py
+++ data/user-simulator.py
@@ -7,3 +7,4 @@ lines = open('test-small.json').read().split("\n")
 for data in lines:
   response = requests.post(url, data=data, headers={"Content-Type": "application/json"})
   print(response)
+  # break
--- falcon/src/bdr.py
+++ falcon/src/bdr.py
@@ -41,69 +41,71 @@ post_request_schema = {
 }
 
 class BdrResource:
-    def on_get(self, req, resp):
-	session = cluster.connect('bdr')
+  def on_get(self, req, resp):
+    session = cluster.connect('bdr')
 
-	input = falcon.uri.parse_query_string(req.query_string)
-	print('input = {}'.format(input))
-        if 'product-kappa' in input:
-	  product = int(input['product-kappa'])
-	  product_lookup = session.prepare("SELECT other_products FROM top_other_products_kappa WHERE product=?")
-	  product_lookup.consistency_level = ConsistencyLevel.ONE
-	  rows = session.execute(product_lookup, [product])
-	  resp.status = falcon.HTTP_200
-          result = []
-          if rows:
-            result = rows[0].other_products
-          resp.body = json.dumps({"product":product, "recommendedProducts":result}, encoding='utf-8')
-        elif 'product-lambda' in input:
-	  product = int(input['product-lambda'])
-	  stream_lookup = session.prepare("SELECT other_products FROM top_other_products_stream WHERE product=?")
-	  stream_lookup.consistency_level = ConsistencyLevel.ONE
-	  stream_rows = session.execute(stream_lookup, [product])
-	  batch_lookup = session.prepare("SELECT other_products FROM top_other_products_batch WHERE product=?")
-	  batch_lookup.consistency_level = ConsistencyLevel.ONE
-	  batch_rows = session.execute(batch_lookup, [product])
-	  resp.status = falcon.HTTP_200
-          result = []
-          if batch_rows and stream_rows:
-  	    merged = []
-            for x in (batch_rows[0].other_products + stream_rows[0].other_products):
-              if x not in merged:
-                merged.append(x)
-            result = merged[:5]
-          elif batch_rows:
-            result = batch_rows[0].other_products          
-          elif stream_rows:
-            result = stream_rows[0].other_products          
-          resp.body = json.dumps({"product":product, "recommendedProducts":result}, encoding='utf-8')
-        elif 'user' in input:
-          user = int(input['user'])
-	  user_lookup = session.prepare("SELECT recommended_products FROM cf WHERE user_id=?")
-	  user_lookup.consistency_level = ConsistencyLevel.ONE
-          rows = session.execute(user_lookup, [user])
-          resp.status = falcon.HTTP_200
-          result = []
-          if rows:
-            result = rows[0].recommended_products
-          resp.body = json.dumps({"user":user, "recommendedProducts":result}, encoding='utf-8')
-	else:
-          resp.status = falcon.HTTP_400
-          resp.body = '{"error": "Parameter *product-lambda*, *product-kappa* or *user* must be provided in the query string"}'
-
-    @request_schema(post_request_schema)
-    def on_post(self, req, resp):
-	msg = json.dumps(req.context['doc'], encoding='utf-8')
-        producer = KafkaProducer(bootstrap_servers='kafka:9092')
-        producer.send('bdr', msg)
-        producer.flush()
-        resp.status = falcon.HTTP_201
-        resp.body = msg
+    input = falcon.uri.parse_query_string(req.query_string)
+    print('input = {}'.format(input))
+    if 'product-kappa' in input:
+      product = int(input['product-kappa'])
+      product_lookup = session.prepare("SELECT other_products FROM top_other_products_kappa WHERE product=?")
+      product_lookup.consistency_level = ConsistencyLevel.ONE
+      rows = session.execute(product_lookup, [product])
+      resp.status = falcon.HTTP_200
+      result = []
+      if rows:
+        result = rows[0].other_products
+      resp.body = json.dumps({"product":product, "recommendedProducts":result})
+    elif 'product-lambda' in input:
+      product = int(input['product-lambda'])
+      stream_lookup = session.prepare("SELECT other_products FROM top_other_products_stream WHERE product=?")
+      stream_lookup.consistency_level = ConsistencyLevel.ONE
+      stream_rows = session.execute(stream_lookup, [product])
+      batch_lookup = session.prepare("SELECT other_products FROM top_other_products_batch WHERE product=?")
+      batch_lookup.consistency_level = ConsistencyLevel.ONE
+      batch_rows = session.execute(batch_lookup, [product])
+      resp.status = falcon.HTTP_200
+      result = []
+      if batch_rows and stream_rows:
+        merged = []
+        for x in (batch_rows[0].other_products + stream_rows[0].other_products):
+          if x not in merged:
+            merged.append(x)
+        result = merged[:5]
+      elif batch_rows:
+        result = batch_rows[0].other_products     
+      elif stream_rows:
+        result = stream_rows[0].other_products          
+      resp.body = json.dumps({"product":product, "recommendedProducts":result, "from_stream":stream_rows[0].other_products if stream_rows else [], "from_batch":batch_rows[0].other_products if batch_rows else []})
+    elif 'user' in input:
+      user = int(input['user'])
+      user_lookup = session.prepare("SELECT recommended_products FROM cf WHERE user_id=?")
+      user_lookup.consistency_level = ConsistencyLevel.ONE
+      rows = session.execute(user_lookup, [user])
+      resp.status = falcon.HTTP_200
+      result = []
+      if rows:
+        result = rows[0].recommended_products
+      resp.body = json.dumps({"user":user, "recommendedProducts":result})
+    else:
+      resp.status = falcon.HTTP_400
+      resp.body = '{"error": "Parameter *product-lambda*, *product-kappa* or *user* must be provided in the query string"}'
+          
+  @request_schema(post_request_schema)
+  def on_post(self, req, resp):
+    msg = json.dumps(req.context['doc'])
+    print('msg = {}'.format(msg))
+    producer = KafkaProducer(bootstrap_servers='kafka:9092')
+    msg = msg.encode('utf-8')
+    producer.send('bdr', msg)
+    producer.flush()
+    resp.status = falcon.HTTP_201
+    resp.body = msg
 
 class HealthCheckResource:
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps({"healthcheck":"ok"}, encoding='utf-8')
+        resp.body = json.dumps({"healthcheck":"ok"})
 
 app = falcon.API(
     middleware=[
